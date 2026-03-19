823a8f0e83c2c47f472d022af8a7bc9113de3606
Diego de Freitas <Diego.deFreitas0725@jala.university>
2026-03-19 15:00:55 -0300

    feat: add tag logic

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,14 @@
 import controller.TaskController;
+import service.TagService;
 import service.TaskService;
 import view.AppView;
+import view.TaskView;
 
 public class Main {
     public static void main(String[] args) {
-        TaskController taskController = new TaskController(new TaskService());
-
-        AppView view = new AppView(taskController);
+        TaskController taskController = new TaskController(new TaskService(), new TagService());
+        TaskView taskView = new TaskView(taskController);
+        AppView view = new AppView(taskController, taskView);
 
         view.run();
     }
